50b22b06c733ba650c5577f1c847707ad53b1c8c
Theodore Messinezis <theo@theomessin.com>
2019-07-11 12:58:01 +0100

    Code list_secrets and fix dependencies. (#3)
    * Code list_secrets and fix dependencies.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,7 @@
-from unittest import mock
+import shutil
+import tempfile
+from os import path
+from unittest import TestCase, mock
 
 import shush
 
@@ -16,7 +19,7 @@ def test_initialize_creates_key_ring(
     KeyManagementServiceClient,  # noqa: N803 to use CamelCase for class.
     CloudStorageClient,  # noqa: N803 to use CamelCase for class.
 ):
-    key_ring_id = "shush-secrets"
+    key_ring_id = "shush-keyring"
     project_id = SHUSH_CLIENT_PARAMS["project_id"]
     location_id = SHUSH_CLIENT_PARAMS["keyring_location"]
 
@@ -32,7 +35,7 @@ def test_initialize_creates_key_ring(
     # Check KMS method was called once with appropriate parameters.
     # https://github.com/GoogleCloudPlatform/python-docs-samples/blob/950d97a9e783165731c52c3688b743e2f3032209/kms/api-client/snippets.py#L17-L38
     client.create_key_ring.\
-        assert_called_once_with(parent, "shush-secrets", keyring)
+        assert_called_once_with(parent, key_ring_id, keyring)
 
 
 @mock.patch("google.cloud.storage.Client")
@@ -41,7 +44,7 @@ def test_initialize_creates_default_crypto_key(
     KeyManagementServiceClient,  # noqa: N803 to use CamelCase for class.
     CloudStorageClient,  # noqa: N803 to use CamelCase for class.
 ):
-    key_ring_id = "shush-secrets"
+    key_ring_id = "shush-keyring"
     project_id = SHUSH_CLIENT_PARAMS["project_id"]
     location_id = SHUSH_CLIENT_PARAMS["keyring_location"]
 
@@ -53,7 +56,7 @@ def test_initialize_creates_default_crypto_key(
 
     from google.cloud.kms_v1 import enums
     purpose = enums.CryptoKey.CryptoKeyPurpose.ENCRYPT_DECRYPT
-    crypto_key = {'purpose': purpose}
+    crypto_key = {"purpose": purpose}
 
     # Check KMS method was called once with appropriate parameters.
     # https://github.com/GoogleCloudPlatform/python-docs-samples/blob/29e4cd6c236567ec35b7fb68ab78823a180b2594/kms/api-client/snippets.py#L41-L63
@@ -83,3 +86,30 @@ def test_initialize_creates_bucket(
     bucket.create.assert_called_once_with(
         project=project_id, location=location_id,
     )
+
+
+class TestWriteSecretMethods(TestCase):
+    def setUp(self):
+        # Patch Google Cloud libraries.
+        # https://docs.python.org/3/library/unittest.mock.html#patch-methods-start-and-stop
+        mock.patch("google.cloud.storage.Client").start()
+        mock.patch("google.cloud.kms_v1.KeyManagementServiceClient").start()
+
+        # Create a temporary directory and file for testing.
+        self.directory = tempfile.mkdtemp()
+        self.secret_file = path.join(self.directory, "secret.tmp")
+
+        f = open(self.secret_file, "w")
+        f.write("correct horse battery staple")
+
+    def tearDown(self):
+        # Undo all patches.
+        mock.patch.stopall()
+        # Remove temporary directory.
+        shutil.rmtree(self.directory)
+
+    def test_opened_file_not_binary_raises_error(self):
+        client = shush.Client(**SHUSH_CLIENT_PARAMS)
+        secret = open(self.secret_file, "r")
+        with self.assertRaises(ValueError):
+            client.write_secret_from_file("secret", secret)
